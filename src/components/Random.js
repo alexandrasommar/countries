6c98a039dbf203639ebd23044009c02c39cc5a83
Alexandra Sommar <alexandra@sommar.se>
2017-06-10 10:53:12 +0200

    Edited random facts

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,10 +1,44 @@
 const React = require('react');
 
-export default function Random () {
-    return (
-        <div>
-        <p>Search</p>
-        <input type="text" />
-        </div>
-    )
+class Random extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            random: []
+        }
+    }
+
+    getRandomNumber (min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    componentDidMount () {
+        fetch('https://restcountries.eu/rest/v2/name/united')
+        .then(response => response.json())
+        .then(data => {
+            this.setState({ random: data })
+        });
+    }
+    render () {
+
+        let facts = this.state.random.map(( item, index) => {
+            return <ul key={index}>
+                        <li>{item.name} has a poinpulation of {item.population}</li>
+                        <li>Their nativename is {item.nativeName}</li>
+                        <li>And their top level domain is {item.topLevelDomain.map( (level, index) =>{
+                            return <span key={index}>{level}</span>
+                            })}?
+                        </li>
+                    </ul>
+        })
+
+        return (
+            <div>
+                <h2>Did you know?</h2>
+                {facts}
+            </div>
+        )
+    }
 }
+
+export default Random;
